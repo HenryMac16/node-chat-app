e9e71ef9f9cbb2e4b2407739a008507cc6e4ebb1
Henry Macarthur <henrymacarthur@s-149-142-27-222.resnet.ucla.edu>
2019-02-17 20:23:51 -0800

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000; //for heroku
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server); //can accept websocket connections
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//register an event listener
+//socket argument represents the individual socket
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+
+  socket.on('disconnect', (socket) => {
+    console.log('User disconnected');
+  })
+});
+
+
+//app
+server.listen(port, () => { //this calls create server, passes app in as arguement
   console.log(`Server is up on port ${port}`);
 });
