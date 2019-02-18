350039a99d6c20ebcd3aebc0ce21a964bb02ddca
Henry Macarthur <henrymacarthur@s-149-142-27-222.resnet.ucla.edu>
2019-02-17 21:33:51 -0800

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -24,6 +24,18 @@ io.on('connection', (socket) => {
 //     text: "Hello!",
 //     createdAt: 123
 //   });
+socket.emit('newMessage', {
+  from: 'Admin',
+  text: 'Welcome to the chat app',
+  createdAt: new Date().getTime()
+});
+
+//everyone but current user
+socket.broadcast.emit('newMessage', {
+  from: 'Admin',
+  text: 'New user joined',
+  createdAt: new Date().getTime()
+});
 
 
 socket.on('createMessage', (message) => {
@@ -33,6 +45,11 @@ socket.on('createMessage', (message) => {
     text: message.text,
     createdAt: new Date().getTime()
   });
+  // socket.broadcast.emit('newMessage', {
+  //   from: message.from,
+  //   text: message.text,
+  //   createdAt: new Date().getTime()
+  // })
 });
 
 
